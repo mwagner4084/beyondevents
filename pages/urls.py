b828b1670485548befea8baa0c81c5a853552a98
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-09-25 14:01:39 -0400

    add page urls and views add templatetags add testing for homepage

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import (AboutPageView, ContactPageView, HomePageView,
+                    PricingPageView, TestimonialsPageView)
 
 urlpatterns = [
-path("", HomePageView.as_view(), name="home"),
+    path("testimonials/", TestimonialsPageView.as_view(), name="testimonials"),
+    path("pricing/", PricingPageView.as_view(), name="pricing"),
+    path("contact/", ContactPageView.as_view(), name="contact"),
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("", HomePageView.as_view(), name="home"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,16 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = "home.html"
+    template_name = "index.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+class PricingPageView(TemplateView):
+    template_name = "pricing.html"
+
+class TestimonialsPageView(TemplateView):
+    template_name = "testimonials.html"
+
+class ContactPageView(TemplateView):
+    template_name = "contact.html"
