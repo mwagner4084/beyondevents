d45c3673b585f2017b7391820e2955e5ce457eef
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-10-01 15:44:32 -0400

    add content to contact template create contact form and model

---
+++ pages/forms.py
@@ -0,0 +1,65 @@
+from crispy_bootstrap5.bootstrap5 import FloatingField
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
+from django import forms
+
+
+class ContactForm(forms.Form):
+    """ Form for the contact page. """
+    fname = forms.CharField(
+        help_text='',
+        label='First Name',
+        max_length=200,
+        widget=forms.TextInput()
+    )
+    lname = forms.CharField(
+        help_text='',
+        label='Last Name',
+        max_length=200,
+        widget=forms.TextInput()
+    )
+    email = forms.EmailField(
+        help_text='',
+        label='Email Address',
+        max_length=200,
+        widget=forms.EmailInput()
+    )
+    phone = forms.CharField(
+        help_text='',
+        label='Phone Number',
+        max_length=200,
+        widget=forms.TextInput()
+    )
+    wedding_date = forms.DateField(
+        help_text='',
+        label='Wedding Date (MM/DD/YYYY)',
+        widget=forms.DateInput()
+    )
+    comments = forms.CharField(
+        help_text='',
+        label='Comments',
+        max_length=500,
+        widget=forms.TextInput()
+    )
+    referred_by = forms.CharField(
+        help_text='',
+        label='How did you hear about us?',
+        max_length=200,
+        widget=forms.TextInput()
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-contactForm'
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            FloatingField('fname'),
+            FloatingField('lname'),
+            FloatingField('email'),
+            FloatingField('phone'),
+            FloatingField('wedding_date'),
+            FloatingField('comments'),
+            FloatingField('referred_by'),
+            Submit('submit', 'Submit')
+        )
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Contact(models.Model):
+    """ Contact Model """
+    fname = models.CharField(max_length=254, null=True, blank=True)
+    lname = models.CharField(max_length=254, null=True, blank=True)
+    email = models.EmailField(max_length=254, unique=True)
+    phone = models.CharField(max_length=254, null=True, blank=True)
+    wedding_date = models.DateField(auto_now_add=True)
+    comments = models.CharField(max_length=500, null=True, blank=True)
+    referred_by = models.CharField(max_length=254, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    contacted = models.BooleanField(default=False)
+    notes = models.CharField(max_length=5000, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.lname}, {self.fname}"
+
+    def get_absolute_url(self):
+        return reverse("contact", args=(self.pk))
