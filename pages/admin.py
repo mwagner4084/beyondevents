81dffcb34a0d9e53b29f994fad30b3ec13d2e033
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-10-02 14:47:26 -0400

    register contact model to admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .forms import ContactForm
+from .models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    form = ContactForm
+    model = Contact
+
+    list_display = ('fname', 'lname', 'email', 'phone', 'location', 'wedding_date',
+                    'comments', 'referred_by', 'date_added', 'contacted', 'notes')
+    list_filter = ('contacted', 'date_added', 'wedding_date')
+    search_fields = ('fname', 'lname', 'email', 'phone', 'location',
+                     'wedding_date', 'comments', 'referred_by', 'notes')
+    ordering = ('-date_added',)
+
+
+admin.site.register(Contact, ContactAdmin)
