e98731bd7d6df9ce7f285a904a4ff91b4aa354fc
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-10-14 22:42:21 -0400

    fix navbar responsiveness fix margins and padding remove heading from testimonials page

--- static/js/base.js
+++ static/js/base.js
@@ -1,3 +1,16 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('show');
+function toggleMobileMenu(menu) {
+  menu.classList.toggle('open');
+}
+
+const hamburger = document.getElementById('hamburger-icon');
+if (hamburger) {
+  hamburger.addEventListener('click', () => {
+    toggleMobileMenu(hamburger);
   });
+}
+
+$(document).ready(function(){
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+	});
+});
