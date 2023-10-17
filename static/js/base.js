1b7e2af9b6b6e828babd8db43cb85c2823324fdf
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-10-16 23:37:19 -0400

    update page format update logo update navbar icon update navbar css

--- static/js/base.js
+++ static/js/base.js
@@ -2,21 +2,8 @@ function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
 }
 
-const hamburger = document.getElementById('hamburger-icon');
-if (hamburger) {
-  hamburger.addEventListener('click', () => {
-    toggleMobileMenu(hamburger);
-  });
-}
-
 $(document).ready(function(){
-	$('#nav-icon').click(function(){
-		$(this).toggleClass('open');
+	$('#menu-icon').click(function(){
+		$('.site-header').toggleClass('open');
 	});
-});
-
-$(document).ready(function(){
-  $('#nav-icon').click(function(){
-    toggleMobileMenu($('.mobile-menu'));
-  });
 });
